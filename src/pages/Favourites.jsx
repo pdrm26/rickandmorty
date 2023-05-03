ef3a5d8393307ae712021c039b48f0a86897955b
ped <peeeedii@gmail.com>
2023-05-03 23:16:43 +0330

    feat: add all favourites list page

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,20 @@
+import CharacterCard from "components/CharacterCard";
+import { useSelector } from "react-redux";
+import styles from "./Favourites.module.css";
+
+export default function Favourites() {
+  const allFavCharacters = useSelector((state) => state.favouriteCharacters);
+  console.log(allFavCharacters);
+  return (
+    <section className={styles["favourite-characters-section"]}>
+      {!allFavCharacters.length && <p className={styles["dont-have-favourite"]}>You dont have any favourite characters right now.</p>}
+      {allFavCharacters.map((character) => (
+        <CharacterCard
+          character={character}
+          key={character.id}
+          isFavouriteCharacter={true}
+        />
+      ))}
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import CharactersList from "../components/CharactersList";
+import FavouritesList from "../components/FavouritesList";
 
 export default function Home() {
-
-
   return (
     <>
+      <FavouritesList />
       <CharactersList />
     </>
   );
